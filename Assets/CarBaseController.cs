6870bdcc52ebb43aea6e7995794d51ff53a240b8
Gabriel Morin <Gaby2796@Hotmail.fr>
2015-03-07 12:15:31 +0100

    voiture base

---
+++ Assets/CarBaseController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class AxleInfo {
+	public WheelCollider leftWheel;
+	public WheelCollider rightWheel;
+	public bool motor;
+	public bool steering;
+}
+
+public class CarBaseController : MonoBehaviour {
+	public List<AxleInfo> axleInfos; 
+	public float maxMotorTorque;
+	public float maxSteeringAngle;
+
+	private Quaternion cylRotationRight =  Quaternion.AngleAxis (270, Vector3.right);
+	private Quaternion cylRotationUp =  Quaternion.AngleAxis (90, Vector3.forward);
+	
+	// finds the corresponding visual wheel
+	// correctly applies the transform
+	public void ApplyLocalPositionToVisuals(WheelCollider collider)
+	{
+		if (collider.transform.childCount == 0) {
+			return;
+		}
+
+		Transform visualWheel = collider.transform.GetChild(0);
+
+		Vector3 position;
+		Quaternion rotation;
+		collider.GetWorldPose(out position, out rotation);
+
+		visualWheel.transform.position = position;
+		visualWheel.transform.rotation = rotation*cylRotationRight*cylRotationUp;
+	}
+	
+	public void FixedUpdate()
+	{
+		float motor = maxMotorTorque * Input.GetAxis("Vertical");
+		float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+		
+		foreach (AxleInfo axleInfo in axleInfos) {
+			if (axleInfo.steering) {
+				axleInfo.leftWheel.steerAngle = steering;
+				axleInfo.rightWheel.steerAngle = steering;
+			}
+			if (axleInfo.motor) {
+				axleInfo.leftWheel.motorTorque = motor;
+				axleInfo.rightWheel.motorTorque = motor;
+			}
+			ApplyLocalPositionToVisuals(axleInfo.leftWheel);
+			ApplyLocalPositionToVisuals(axleInfo.rightWheel);
+		}
+	}
+}

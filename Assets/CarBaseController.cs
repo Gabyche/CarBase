092b2aee845484c097eca4a82950e45497c87983
Gabriel Morin <Gaby2796@Hotmail.fr>
2015-03-07 18:35:21 +0100

    refactoring CarBase

--- Assets/CarBaseController.cs
+++ Assets/CarBaseController.cs
@@ -20,57 +20,23 @@ public class CarBaseController : MonoBehaviour {
 
 	private Quaternion cylRotationRight =  Quaternion.AngleAxis (270, Vector3.right);
 	private Quaternion cylRotationUp =  Quaternion.AngleAxis (90, Vector3.forward);
-	
-	// finds the corresponding visual wheel
-	// correctly applies the transform
-	public void ApplyLocalPositionToVisuals(WheelCollider collider)
-	{
-		if (collider.transform.childCount == 0) {
-			return;
-		}
 
-		Transform visualWheel = collider.transform.GetChild(0);
-
-		Vector3 position;
-		Quaternion rotation;
-		collider.GetWorldPose(out position, out rotation);
-
-		visualWheel.transform.position = position;
-		visualWheel.transform.rotation = rotation*cylRotationRight*cylRotationUp;
-	}
-	
 	public void FixedUpdate()
 	{
-		float brake=0;
-		float motor=0;
-
-		if (Input.GetAxis ("Vertical") < 0 && GoForward()) {
-			 brake = - maxBrakeTorque * Input.GetAxis ("Vertical");
-		}
-		else {
-			 motor = maxMotorTorque * Input.GetAxis ("Vertical");
-			if ( GoRewind() ) {
-				motor = motor/1.5f;
-			}
-		}
-
-		float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
 		
 		foreach (AxleInfo axleInfo in axleInfos) {
 			if (axleInfo.steering) {
-				axleInfo.leftWheel.steerAngle = steering;
-				axleInfo.rightWheel.steerAngle = steering;
+				axleInfo.leftWheel.steerAngle = Steer();
+				axleInfo.rightWheel.steerAngle = Steer();
 			}
 			if (axleInfo.motor) {
-				axleInfo.leftWheel.motorTorque = motor;
-				axleInfo.rightWheel.motorTorque = motor;
+				axleInfo.leftWheel.motorTorque = Motor();
+				axleInfo.rightWheel.motorTorque = Motor();
 			}
 			if (axleInfo.brake){
-				axleInfo.leftWheel.brakeTorque = brake;
-				axleInfo.rightWheel.brakeTorque = brake;
+				axleInfo.leftWheel.brakeTorque = Brake();
+				axleInfo.rightWheel.brakeTorque = Brake();
 			}
-
-
 
 			ApplyLocalPositionToVisuals(axleInfo.leftWheel);
 			ApplyLocalPositionToVisuals(axleInfo.rightWheel);
@@ -78,12 +44,65 @@ public class CarBaseController : MonoBehaviour {
 		}
 	}
 
+	public void ApplyLocalPositionToVisuals(WheelCollider collider)
+	{
+		if (collider.transform.childCount == 0) {
+			return;
+		}
+		
+		Transform visualWheel = collider.transform.GetChild(0);
+		
+		Vector3 position;
+		Quaternion rotation;
+		collider.GetWorldPose(out position, out rotation);
+		
+		visualWheel.transform.position = position;
+		visualWheel.transform.rotation = rotation*cylRotationRight*cylRotationUp;
+	}
+
+
+
+	public float Steer () {
+		return maxSteeringAngle * Horizontal();
+	}
+
+	public float Brake() {
+		if (Braking()) {
+			return - maxBrakeTorque * Vertical();
+		}
+		return 0;
+	}
+
+	public float Motor() {
+		float motor;
+		if (!Braking()) {
+			motor = maxMotorTorque * Vertical();
+			if ( GoRewind() ) {
+				motor = motor/1.2f;
+			}
+			return motor;
+		}
+		return 0;
+	}
+
+	public bool Braking() {
+		return Vertical() < 0 && GoForward ();
+	}
+
 	public bool GoForward() {
 		AxleInfo axle = axleInfos [0];
 		return (axle.leftWheel.rpm > 0);
 	}
-
+	
 	public bool GoRewind() {
 		return !GoForward();
+	}
+	
+	public float Vertical() {
+		return Input.GetAxis ("Vertical");
+	}
+	
+	public float Horizontal() {
+		return Input.GetAxis ("Horizontal");
 	}
 }
